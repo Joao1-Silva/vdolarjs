ee9c64da9a41023da87423879f7dc1c73da71f74
Jonathan Silva <jonathansilvads@hotmail.com>
2023-09-14 00:14:29 -0400

    test 11

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import consultaDolar from 'consulta-dolar-venezuela';
 import cors from 'cors'
 
 const app = express()
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(cors())
 
